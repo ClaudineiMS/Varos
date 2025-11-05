270ef65eb586b447f9e2428a0abc72f327cff64a
Claudinei <claudineisilva.ti@fundecc.org.br>
2025-11-05 10:28:20 -0300

    feat(Filters): Ajuste na cor de fundo dos inputs

--- src/components/Filters.tsx
+++ src/components/Filters.tsx
@@ -6,16 +6,19 @@ export default function Filters(): JSX.Element {
       <div className="flex flex-wrap items-center gap-3 bg-lightDark p-5 rounded-xl">
         Nome do consultor:
         <input
-          className="bg-dark px-3 py-2 rounded-sm text-sm"
+          className="bg-[#3a3a3a] px-3 py-2 rounded-sm text-sm text-white placeholder-gray-400"
           placeholder="Nome"
         />
         Email do consultor:
         <input
-          className="bg-dark px-3 py-2 rounded-sm text-sm"
+          className="bg-[#3a3a3a] px-3 py-2 rounded-sm text-sm text-white placeholder-gray-400"
           placeholder="Email"
         />
         Período:
-        <input className="bg-dark px-3 py-2 rounded-sm text-sm" type="date" />
+        <input
+          className="bg-[#3a3a3a] px-3 py-2 rounded-sm text-sm text-white placeholder-gray-400"
+          type="date"
+        />
       </div>
     </div>
   );
