d0d1e6471733ccc57b58d1fb940f27d7f14b9ea7
Claudinei <claudineimoreirax7l@gmail.com>
2025-11-05 22:05:16 -0300

    fix(table): Ajuste do contorno da tabela

--- src/components/dashboard/Table.tsx
+++ src/components/dashboard/Table.tsx
@@ -6,8 +6,8 @@ import { User } from "@/types/User";
 
 export default function Table({ users }: { users: User[] }): JSX.Element {
   return (
-    <div className="mt-2 h-[55vh] w-full bg-[#1e1e1e] border border-[rgba(255,255,255,0.08)] rounded-sm  shadow-md backdrop-blur-sm transition hover:bg-[#333333]">
-      <div className="overflow-x-auto mt-6 bg-lightDark rounded-2xl">
+    <div className="mt-2 w-full bg-[#1e1e1e] border border-[rgba(255,255,255,0.08)] rounded-sm shadow-md transition hover:bg-[#333333]">
+      <div className="overflow-y-auto h-[55vh] bg-[#1e1e1e]/60 backdrop-blur-sm rounded-sm">
         <table className="w-full text-sm">
           <thead>
             <tr className="text-left border-b border-dark">
@@ -21,7 +21,10 @@ export default function Table({ users }: { users: User[] }): JSX.Element {
                 "Criado em",
                 "Atualizado em",
               ].map((header) => (
-                <th key={header} className="px-4 py-3">
+                <th
+                  key={header}
+                  className="px-4 py-3 sticky top-0 bg-[#1e1e1e]"
+                >
                   {header}
                 </th>
               ))}
