9f6aa435b0ebc40f5da651c1d2f8a87bbc33ada2
Claudinei <claudineisilva.ti@fundecc.org.br>
2025-11-06 17:08:32 -0300

    fix(filter): Correção do filtro

--- src/components/dashboard/Filters.tsx
+++ src/components/dashboard/Filters.tsx
@@ -8,12 +8,25 @@ interface FiltersProps {
   };
   // eslint-disable-next-line no-unused-vars
   setFilters: (filters: { nome: string; email: string; data: string }) => void;
+  fetchAllUsers?: () => void;
 }
 
 export default function Filters({
   filters,
   setFilters,
+  fetchAllUsers,
 }: FiltersProps): JSX.Element {
+  const handleFilterChange = (
+    field: keyof typeof filters,
+    value: string
+  ): void => {
+    setFilters({ ...filters, [field]: value });
+    if (value === "" && fetchAllUsers) {
+      setFilters({ nome: "", email: "", data: "" });
+      fetchAllUsers();
+    }
+  };
+
   return (
     <div className="mt-2 w-full border border-[rgba(255,255,255,0.08)] rounded-sm shadow-md backdrop-blur-sm transition hover:bg-[#333333]">
       <div className="flex flex-wrap items-center gap-3 bg-lightDark p-5 rounded-xl">
@@ -22,21 +35,21 @@ export default function Filters({
           className="bg-[#3a3a3a] px-3 py-2 rounded-sm text-sm text-white placeholder-gray-400"
           placeholder="Nome"
           value={filters.nome}
-          onChange={(e) => setFilters({ ...filters, nome: e.target.value })}
+          onChange={(e) => handleFilterChange("nome", e.target.value)}
         />
         Email do consultor:
         <input
           className="bg-[#3a3a3a] px-3 py-2 rounded-sm text-sm text-white placeholder-gray-400"
           placeholder="Email"
           value={filters.email}
-          onChange={(e) => setFilters({ ...filters, email: e.target.value })}
+          onChange={(e) => handleFilterChange("email", e.target.value)}
         />
         Período:
         <input
           className="bg-[#3a3a3a] px-3 py-2 rounded-sm text-sm text-white placeholder-gray-400"
           type="date"
           value={filters.data}
-          onChange={(e) => setFilters({ ...filters, data: e.target.value })}
+          onChange={(e) => handleFilterChange("data", e.target.value)}
         />
       </div>
     </div>
